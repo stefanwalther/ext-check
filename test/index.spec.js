58c725ba02ce8f98e63bdc9a0ded7326441c928a
Stefan Walther <swr@qlik.com>
2015-09-24 21:45:18 +0200

    listDir

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,7 +3,7 @@
 var path = require( 'path' );
 var expect = require( 'chai' ).expect;
 var ExtensionCheck = require( './../lib/ExtensionCheck' );
-var fs = require('fs-extra');
+var fs = require( 'fs-extra' );
 //var rimraf = require( 'rimraf' );
 //var zipLib = require( 'adm-zip' );
 
@@ -14,11 +14,12 @@ describe( 'ext-check', function () {
         "sampleSrc": './test/fixtures/extensions/sample.zip',
         "sample": './test/.tmp/sample.zip',
         "non_existing_sample": './test/.tmp/not_existing_sample.zip',
-        "existing_not_zip": './test/fixtures/extensions/sample.7z'
+        "existing_not_zip": './test/fixtures/extensions/sample.7z',
+        "listDir": './test/fixtures/listDir'
     };
     beforeEach( function ( done ) {
         ec = new ExtensionCheck();
-        fs.copySync(path.resolve(fixtures.sampleSrc), path.resolve( fixtures.sample));
+        fs.copySync( path.resolve( fixtures.sampleSrc ), path.resolve( fixtures.sample ) );
         done();
     } );
 
@@ -27,7 +28,7 @@ describe( 'ext-check', function () {
         it( 'should throw an exception for non existing files', function ( done ) {
             ec.list( 'c:\\does_not_exist.zip', function ( err, data ) {
                 expect( err ).to.exist;
-                expect( err.errName ).to.be.equal('FILE_NOT_EXISTS');
+                expect( err.errName ).to.be.equal( 'FILE_NOT_EXISTS' );
                 done();
             } );
         } );
@@ -74,16 +75,16 @@ describe( 'ext-check', function () {
             var fileToCheck = path.resolve( fixtures.existing_not_zip );
             ec.check( fileToCheck, function ( err, checkResult ) {
                 expect( err ).to.exist;
-                expect( err.errName ).to.be.equal('NOT_A_ZIP_FILE');
+                expect( err.errName ).to.be.equal( 'NOT_A_ZIP_FILE' );
                 done();
             } );
-        });
+        } );
 
         it( 'should throw an exception if the file doesn\'t exist', function ( done ) {
             var fileToCheck = path.resolve( fixtures.non_existing_sample );
             ec.check( fileToCheck, function ( err, checkResult ) {
                 expect( err ).to.exist;
-                expect( err.errName ).to.be.equal('FILE_NOT_EXISTS');
+                expect( err.errName ).to.be.equal( 'FILE_NOT_EXISTS' );
                 done();
             } );
         } );
@@ -126,13 +127,82 @@ describe( 'ext-check', function () {
 
         it.skip( 'creates a backup', function ( done ) {
 
-            ec.fix( path.resolve( fixtures.sample), true, function ( err, fixResult ) {
+            ec.fix( path.resolve( fixtures.sample ), true, function ( err, fixResult ) {
                 expect( err ).to.be.undefined;
-                expect( path.exists( fixResults.backupFile) ).to.be.true;
+                expect( path.exists( fixResults.backupFile ) ).to.be.true;
+                done();
+            } );
+        } );
+
+        it.skip( 'removes unsupported files', function ( done ) {
+            expect( true ).to.equal( false );
+            done();
+        } );
+
+        it.skip( 'keeps files which are supported', function ( done ) {
+            expect( true ).to.equal( false );
+            done();
+        } );
+
+        it.skip( 'removes unsupported directories', function ( done ) {
+            expect( true ).to.equal( false );
+            done();
+        } );
+
+    } );
+
+    describe( 'getZipsInDir', function () {
+
+        it( 'returns an error if the dir is not existing', function ( done ) {
+            ec.getZipsInDir( 'c:\\non_existing_folder', function ( err, zipFiles ) {
+                expect( err ).to.exist;
+                expect( err.errName ).to.be.equal( 'DIR_NOT_EXISTS' );
+                done();
+            } );
+        } );
+
+        it( 'returns all the zip files in a dir', function ( done ) {
+            ec.getZipsInDir( path.resolve( fixtures.listDir ), function ( err, zipFiles ) {
+                expect( err ).to.not.exist;
+                expect( zipFiles ).to.be.an.array;
+                expect( zipFiles ).to.be.of.length( 3 );
+                done();
+            } );
+        } );
+
+    } );
+
+    describe( 'listDir', function () {
+
+        it( 'should return an error if the path doesn\'t exist.', function ( done ) {
+            var p = 'C:\\non_existing_dir';
+            ec.listDir( p, function ( err, listDirResult ) {
+                expect( err ).to.exist;
+                expect( err.errName ).to.be.equal( 'DIR_NOT_EXISTS' );
+                done();
+            } );
+        } );
+
+        it( 'should return the results for all .zip files', function ( done ) {
+            ec.listDir( path.resolve( fixtures.listDir ), function ( err, listDirResults ) {
+                expect( err ).to.not.exist;
+                expect( listDirResults ).to.be.an.array;
+                expect( listDirResults.length ).to.be.equal( 3 );
+                done();
+            } );
+        } );
+
+        it( 'should return the listResults', function ( done ) {
+            ec.listDir( path.resolve( fixtures.listDir ), function ( err, listDirResults ) {
+                expect( err ).to.not.exist;
+                expect( listDirResults ).to.be.an.array;
+                expect( listDirResults ).to.have.length(3);
+                expect( listDirResults[0].fileChecked ).to.include('htm.zip');
+                expect( listDirResults[1].fileChecked ).to.include('md.zip');
+                expect( listDirResults[2].fileChecked ).to.include('yml.zip');
                 done();
             });
         } );
 
     } );
-
 } );
