1835d6700c95bc87e5e7fdbcfcef63585ac27254
Stefan Walther <swr@qlik.com>
2015-09-26 00:19:06 +0200

    Basic skeleton for cli tests

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -4,4 +4,34 @@ var expect = require( 'chai' ).expect;
 
 describe( 'cli', function () {
 
+    describe( 'help', function () {
+
+    } );
+
+    describe( 'list', function () {
+
+    } );
+
+    describe( 'listdir', function () {
+
+    } );
+
+    describe( 'listdetails', function () {
+
+    } );
+
+    describe( 'fix', function () {
+
+        it.skip( 'creates a backup file', function ( done ) {
+            expect( true ).to.equal( false );
+            done();
+        } );
+
+        it.skip( 'does not create a backup file with --backup=false', function ( done ) {
+            expect( true ).to.equal( false );
+            done();
+        } );
+
+
+    } );
 } );
